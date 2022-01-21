d60c3cb154f6fd639483475b98391b16bc9b6626
Linyingqing <37841987+Linyingqing@users.noreply.github.com>
2022-01-21 22:28:47 +0800

    Update result_bar.py

--- result_bar.py
+++ result_bar.py
@@ -29,19 +29,19 @@ TOP200_Muff_Sum = [118 , 117, 127]
 index = np.arange(len(labels))
 bar_width = 0.15
 
-ax.bar(index,TOP200_CERW, bar_width,color='white', edgecolor='g', hatch=patterns[0], label='Top200_CERW')
+ax.bar(index,TOP200_CERW, bar_width,color='white', edgecolor='g', hatch=patterns[0], label='Top200_ICE')
 ax.bar(index+bar_width,TOP200_EntroRank, bar_width,color='white', edgecolor='g', hatch=patterns[1], label='Top200_EntroRank')
 ax.bar(index+2*bar_width,TOP200_Dawnrank, bar_width,color='white', edgecolor='g', hatch=patterns[2], label='Top200_Dawnrank')
 ax.bar(index+3*bar_width,TOP200_Muff_Max, bar_width,color='white', edgecolor='g', hatch=patterns[3], label='Top200_Muff_Max')
 ax.bar(index+4*bar_width,TOP200_Muff_Sum, bar_width,color='white', edgecolor='g', hatch=patterns[4], label='Top200_Muff_Sum')
 
-ax.bar(index,TOP100_CERW, bar_width,color='white', edgecolor='b', hatch=patterns[0], label='TOP100_CERW')
+ax.bar(index,TOP100_CERW, bar_width,color='white', edgecolor='b', hatch=patterns[0], label='TOP100_ICE')
 ax.bar(index+bar_width,TOP100_EntroRank, bar_width,color='white', edgecolor='b', hatch=patterns[1], label='Top100_EntroRank')
 ax.bar(index+2*bar_width,TOP100_Dawnrank, bar_width,color='white', edgecolor='b', hatch=patterns[2], label='Top100_Dawnrank')
 ax.bar(index+3*bar_width,TOP100_Muff_Max, bar_width,color='white', edgecolor='b', hatch=patterns[3], label='Top100_Muff_Max')
 ax.bar(index+4*bar_width,TOP100_Muff_Sum, bar_width,color='white', edgecolor='b', hatch=patterns[4], label='Top100_Muff_Sum')
 
-ax.bar(index,TOP50_CERW, bar_width,color='white', edgecolor='r', hatch=patterns[0], label='TOP50_CERW')
+ax.bar(index,TOP50_CERW, bar_width,color='white', edgecolor='r', hatch=patterns[0], label='TOP50_ICE')
 ax.bar(index+bar_width,TOP50_EntroRank, bar_width,color='white', edgecolor='r', hatch=patterns[1], label='Top50_EntroRank')
 ax.bar(index+2*bar_width,TOP50_Dawnrank, bar_width,color='white', edgecolor='r', hatch=patterns[2], label='Top50_Dawnrank')
 ax.bar(index+3*bar_width,TOP50_Muff_Max, bar_width,color='white', edgecolor='r', hatch=patterns[3], label='Top50_Muff_Max')
